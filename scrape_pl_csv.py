fa5be6dc308ac7e065c8aee0593bd7ea54684ab3
jisoo <jisoooh0202@gmail.com>
2022-03-31 14:28:27 +0700

    still failed to save correct format and values

--- scrape_pl_csv.py
+++ scrape_pl_csv.py
@@ -14,18 +14,18 @@ writer.writerow(['position', 'club_name', 'points'])
 
 tags = ['span', 'td']
 classes = ['value', 'long', 'points']
-scraper = soup.find_all(tags, attrs={'class': classes}, limit=20)
-
+# scraper = soup.find_all(tags, attrs={'class': classes}, limit=20)
+scraper = soup.find_all('tr')
 
 # below code gives only the first place of table repeatedly.
-# for standing in scraper:
-#     position = soup.find('span', {'class': 'value'}).string
-#     club_name = soup.find('span', {'class': 'long'}).string
-#     points = soup.find('td', {'class': 'points'}).string
-#
-#     print(position, club_name, points)
-#
-#     writer.writerow([position, club_name, points])
+for standing in scraper:
+    position = soup.find('span', {'class': 'value'}).string
+    club_name = soup.find('span', {'class': 'long'}).string
+    points = soup.find('td', {'class': 'points'}).string
+
+    print(position, club_name, points)
+
+    writer.writerow([position, club_name, points])
 
 # below code gives none for all.
 # for standing in scraper:
@@ -43,10 +43,10 @@ scraper = soup.find_all(tags, attrs={'class': classes}, limit=20)
 # writer.writerow(data)
 
 # below code gives not in list of three.
-for standing in scraper:
-    data = standing.contents
-    print(data)
-    writer.writerow(data)
+# for standing in scraper:
+#     data = standing.contents
+#     print(data)
+#     writer.writerow(data)
 
 
 # string/get_text() are giving commas between each letter.
