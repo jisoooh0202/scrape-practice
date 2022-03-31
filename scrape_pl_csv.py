9e251ed0d95ab6793cadb23df722151b822d3cf1
jisoo <jisoooh0202@gmail.com>
2022-03-31 17:19:53 +0700

    pl standings save as csv still failed

--- scrape_pl_csv.py
+++ scrape_pl_csv.py
@@ -14,11 +14,13 @@ writer.writerow(['position', 'club_name', 'points'])
 
 tags = ['span', 'td']
 classes = ['value', 'long', 'points']
-# scraper = soup.find_all(tags, attrs={'class': classes}, limit=20)
-scraper = soup.find_all('tr')
+standings = soup.find_all(tags, attrs={'class': classes}, limit=20)
+# standings = soup.find_all('tr')
+# standings = soup.find('tbody', attrs={'class': 'tableBodyContainer isPL'}).find_all('tr', attrs={'data-compseason': '418'}, recursive=False)
 
+print(standings)
 # below code gives only the first place of table repeatedly.
-for standing in scraper:
+for standing in standings:
     position = soup.find('span', {'class': 'value'}).string
     club_name = soup.find('span', {'class': 'long'}).string
     points = soup.find('td', {'class': 'points'}).string
@@ -27,30 +29,30 @@ for standing in scraper:
 
     writer.writerow([position, club_name, points])
 
-# below code gives none for all.
-# for standing in scraper:
-#     position = standing.find('span', {'class': 'value'})
-#     club_name = standing.find('span', {'class': 'long'})
-#     points = standing.find('td', {'class': 'points'})
+# below code gives only the first place.
+# for standing in standings:
+#     position = standing.find('span', {'class': 'value'}).text
+#     club_name = standing.find('span', {'class': 'long'}).text
+#     points = standing.find('td', {'class': 'points'}).text
 #
 #     print(position, club_name, points)
 #
 #     writer.writerow([position, club_name, points])
 
 # below code gives all the tag and classes together.
-# data = [str(standing) for standing in scraper]
+# data = [str(standing) for standing in standings]
 # print(data)
 # writer.writerow(data)
 
 # below code gives not in list of three.
-# for standing in scraper:
+# for standing in standings:
 #     data = standing.contents
 #     print(data)
 #     writer.writerow(data)
 
 
 # string/get_text() are giving commas between each letter.
-# for standing in scraper:
+# for standing in standings:
 #     data = standing.string
 #     print(data)
 #     writer.writerow(data)
